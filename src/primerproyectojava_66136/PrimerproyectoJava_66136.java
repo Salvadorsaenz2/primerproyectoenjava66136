1aa9bfc753c6392dd275da9621381fbeb6487333
salva <salva@LAPTOP-B2KFIIUB>
2021-03-02 16:33:28 -0600

    Primer proyecto en java_66136.

---
+++ src/primerproyectojava_66136/PrimerproyectoJava_66136.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerproyectojava_66136;
+
+/**
+ *
+ * @author salva
+ */
+public class PrimerproyectoJava_66136 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        
+    boolean Eres_estudiante = true;
+        System.out.println("¿Eres estudiante? = " + Eres_estudiante);
+    int cual_es_tu_edad = 18;
+        System.out.println("¿Cual es tu edad? = " + cual_es_tu_edad);
+    char Fecha_de_nacimiento = 4;
+        System.out.println("Fecha de nacimiento = " + Fecha_de_nacimiento);
+          
+    }
+    
+}
